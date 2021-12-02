f9e1aca89d41fa8000bc3a5f365f9465a2dca92a
jacobk825 <94639509+jacobk825@users.noreply.github.com>
2021-12-02 14:02:17 -0500

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -7,6 +7,10 @@ import os
 from colorutils import Color
 import time
 
+name = ''
+savename = ''
+convert_bool = False
+
 def hex_to_rgb(value):
     value = value.lstrip('#')
     lv = len(value)
@@ -21,6 +25,8 @@ class Paint(object):
     DEFAULT_CHOOSE_SIZE= 5
     DEFAULT_OPACITY = 1
     OPACITY_OBJECT_COUNTER = 0
+    CURRENT_MODE = 1
+    CURRENT_OBJECT = []
 
     def __init__(self):
         self.root = Tk()
@@ -28,10 +34,9 @@ class Paint(object):
         self.c = Canvas(self.root, bg='white', width=600, height=600)
         self.c.grid(row=2, columnspan=5)
 
-        self.image1 = PIL.Image.new('RGB', (640, 480), 'white')
-        self.draw = ImageDraw.Draw(self.image1)
+        self.color_button = Button(self.root, text='Brush Color', command=self.choose_color).grid(row=0, column=0)
 
-        self.color_button = Button(self.root, text='Color', command=self.choose_color).grid(row=0, column=0)
+        self.canvas_button = Button(self.root, text='Background Color', command=self.background).grid(row=1, column=0)
 
         self.opacity_label = Label(self.root, text='Opacity').grid(row=1, column=1)
         self.opacity_button = Scale(self.root, from_= 0.00, to=1.00, orient=HORIZONTAL, resolution = .01)
@@ -45,9 +50,11 @@ class Paint(object):
 
         self.save_as_button = Button(self.root, text='Save As', command=self.save_as).grid(row=0, column=3)
 
-        self.save_quit_button = Button(self.root, text='Save and Quit', command=self.save).grid(row=0, column=4)
+        self.undo_button = Button(self.root, text='Undo', command = self.undo).grid(row=1, column=3)
 
-        self.color_button = Button(self.root, text='Quit', command=self.quit).grid(row=1, column=4)
+        self.save_quit_button = Button(self.root, text='Convert to ASCII As', command=self.convert).grid(row=0, column=4)
+
+        self.quit_button = Button(self.root, text='My Own Image', command=self.own_image).grid(row=1, column=4)
 
         self.setup()
         self.root.mainloop()
@@ -57,16 +64,25 @@ class Paint(object):
         self.old_y = None
         self.line_width = self.choose_size_button.get()
         self.color = self.DEFAULT_COLOR
+        self.back_color = "ffffff"
         self.c.bind('<B1-Motion>', self.paint)
         self.c.bind('<ButtonRelease-1>', self.reset)
 
     def choose_color(self):
-        self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
 
-
+    def background(self):
+        self.back_color = askcolor(color=self.color)[1]
+        self.c.create_rectangle((-10, -10, 1000, 1000),fill=self.back_color)
+        self.CURRENT_OBJECT.append(max(self.c.find_all())-1)
 
     def paint(self, event):
+        if self.CURRENT_MODE:
+            if len(self.c.find_all()):
+                self.CURRENT_OBJECT.append(max(self.c.find_all()))
+            else:
+                self.CURRENT_OBJECT = [0]
+        self.CURRENT_MODE = 0
         ids = self.c.find_overlapping(event.x,event.y,event.x,event.y)
         if len(ids)>0:
             index = 0
@@ -98,13 +114,23 @@ class Paint(object):
         self.old_x, self.old_y = None, None
         if len(self.c.find_all()):
             self.OPACITY_OBJECT_COUNTER = max(self.c.find_all())
+        self.CURRENT_MODE = 1
 
-    def save(self):
+    def undo(self):
+        if self.CURRENT_OBJECT != []:
+            for i in range(self.CURRENT_OBJECT[-1]+1,max(self.c.find_all())+1):
+                self.c.delete(i)
+            self.CURRENT_OBJECT = self.CURRENT_OBJECT[:-1]
+
+    def convert(self):
+        global savename, convert_bool
+        convert_bool = True
         x = self.root.winfo_rootx()
         y = self.root.winfo_rooty()
         height = self.root.winfo_height() + y
         width = self.root.winfo_width() + x
-        ImageGrab.grab().crop((x+3, y+70, width-3, height-64)).save(str(os.getcwd())+"/Masterpiece.png")
+        ImageGrab.grab().crop((x+3, y+70, width-3, height-64)).save(str(os.getcwd())+"/Masterpiece_Unga_bunga_don't_use_this_name.png")
+        savename = filedialog.asksaveasfile(defaultextension = ".txt", filetypes = [("TXT file", "*.txt")])
         self.root.destroy()
     
     def save_as(self):
@@ -116,8 +142,55 @@ class Paint(object):
         self.root.update()
         ImageGrab.grab().crop((x+3, y+70, width-3, height-64)).save(name.name)
 
-    def quit(self):
-        self.root.destroy()
+    def own_image(self):
+        global name, convert_bool
+        convert_bool = True
+        name = filedialog.askopenfilename()
+        if name:
+            savename = filedialog.asksaveasfile(defaultextension = ".txt", filetypes = [("TXT file", "*.txt")])
+            self.root.destroy()
 
-if __name__ == '__main__':
-    Paint()
+
+Paint()
+
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return(resized_image)
+
+def grayscale(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)
+
+def main(new_width=100):
+    if name:
+        path = name
+    else:
+        path = str(os.getcwd())+"/Masterpiece_Unga_bunga_don't_use_this_name.png"
+    try:
+        image = Image.open(path)
+    except:
+        print(path, "is not a valid pathname to an image.")
+    new_image_data = pixels_to_ascii(grayscale(resize_image(image)))
+
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+    print(ascii_image)
+
+    with open(savename.name, "w") as fp:
+        fp.write(ascii_image)
+
+if convert_bool:
+    main()
+    if os.path.exists(str(os.getcwd())+"/Masterpiece_Unga_bunga_don't_use_this_name.png"):
+        os.remove(str(os.getcwd())+"/Masterpiece_Unga_bunga_don't_use_this_name.png")
